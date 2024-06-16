ab70cb0429806e9ddd3a7c596429135e8b469492
Wojtek <wojciechkopec@op.pl>
2024-06-17 01:11:38 +0200

    Transform to 1992, merge all data to one file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
-import requests
-import zipfile
 import os
 import re
 import time
+import zipfile
+import requests
 from bs4 import BeautifulSoup
+from pyproj import Transformer
 
 
 def main():
@@ -13,36 +14,40 @@ def main():
     url = 'https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/klimat/'
     save_path = 'download/'
 
-    # correct_csv(path_data, 'Stacje_klimat_utf-8.csv')       # correct Stacje-klimat_utf-8.csv
+    correct_csv(path_data, 'Stacje_klimat_utf-8.csv')       # correct Stacje-klimat_utf-8.csv
 
     links = get_directory_structure(url)
 
     for year in links:
-        #download_zip(url+year+year[:-1]+'_m_k.zip', save_path, year)
+        download_zip(url+year+year[:-1]+'_m_k.zip', save_path, year)
         extract_zip(save_path+year+'data.zip', path_data+'all_years_csv')
-        #print(year)
-        #time.sleep(5)
-    print('done')
-    # download_zip(url+catalog+zip_name, save_path, catalog)
-    # extract_zip(save_path+catalog+'1951_1955_m_k.zip')
+        time.sleep(3)       # delay for safety
 
-    # df_stations_coord = import_stations_coord(path+'correct-'+file_name)
-    # deg_min_sec2deg(df_stations_coord, 'lat')
-    # deg_min_sec2deg(df_stations_coord, 'lon')
-    #
-    # df_stations_measurement = import_stations_measurement(save_path+catalog+'k_m_t_2001.csv')
-    # df_stations_connector = import_stations_connector(path+'wykaz_stacji.csv')
-    #
-    # merged_df = pd.merge(df_stations_coord, df_stations_connector, on='short number')
-    # merged_df = pd.merge(merged_df, df_stations_measurement, on='long number')
-    #
-    # print(merged_df)
-    # new_order = ['short number', 'long number', 'lat', 'lon', 'station', 'location', 'year', 'month',
-    #              'sr_miesiac_temp', 'stat_temp', 'sr_miesiac_wilg', 'stat_wilg',
-    #              'sr_miesiac_wind', 'stat_wind', 'sr_miesiac_cloud', 'stat_cloud']
-    # merged_df = merged_df.reindex(columns=new_order)
-    # merged_df.to_excel('test.xlsx', index=False)
-    # print(merged_df)
+    df_stations_coord = import_stations_coord(path_data+'correct-Stacje_klimat_utf-8.csv')
+    deg_min_sec2deg(df_stations_coord, 'lat')
+    deg_min_sec2deg(df_stations_coord, 'lon')
+
+    transform_coordinates(df_stations_coord)
+
+    df_stations_connector = import_stations_connector(path_data+'wykaz_stacji.csv')
+
+    merged_df = pd.merge(df_stations_coord, df_stations_connector, on='short number')
+
+    files = os.listdir(path_data+'all_years_csv')
+    all_data = []
+    for file in files:
+        df = import_stations_measurement(path_data+'all_years_csv/'+file)
+        all_data.append(df)
+
+    df_all_data = pd.concat(all_data, ignore_index=True)
+
+    merged_df = pd.merge(merged_df, df_all_data, on='long number')
+
+    new_order = ['short number', 'long number', 'x_1992', 'y_1992', 'station', 'location', 'year', 'month',
+                 'sr_miesiac_temp', 'stat_temp', 'sr_miesiac_wilg', 'stat_wilg',
+                 'sr_miesiac_wind', 'stat_wind', 'sr_miesiac_cloud', 'stat_cloud']
+    merged_df = merged_df.reindex(columns=new_order)
+    merged_df.to_excel('result/all_data_merged.xlsx', index=False)
 
 
 def get_directory_structure(url_to_download):
@@ -57,6 +62,12 @@ def get_directory_structure(url_to_download):
             links.append(href)
 
     return links
+
+
+def transform_coordinates(df: pd.DataFrame):
+    transformer = Transformer.from_crs('EPSG:4326', 'EPSG:2180')
+    df['x_1992'], df['y_1992'] = transformer.transform(df['lon'], df['lat'])
+    del df['lon'], df['lat']
 
 
 def deg_min_sec2deg(df: pd.DataFrame, begin: str):
@@ -105,12 +116,14 @@ def extract_zip(path_to_zip: str, path_to_save):
     """
     Extract zip file from path_to_zip and saves in path_to_save directory
     :param path_to_zip:
+    :param path_to_save
     :return: None
     """
     with zipfile.ZipFile(path_to_zip, 'r') as zip_ref:
         for file_info in zip_ref.infolist():
             if 'k_m_t' in file_info.filename:
                 zip_ref.extract(file_info, path_to_save)
+                print(f'Extracted {file_info.filename} to {path_to_save}')
 
     return None
 
@@ -197,7 +210,7 @@ def index_of_start_data(list_line: list) -> int:
     """
     return the index of item -------- or KKKKKKKK etc.
     :param list_line:
-    :return: index in list
+    :return: index in list, -1 if not found
     """
     for item in list_line:
         if not re.match(r'^[KN\-]+$', item):
